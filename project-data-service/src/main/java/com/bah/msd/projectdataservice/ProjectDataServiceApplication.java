76765f98e694be6fc5ff5d9628ca7d3af1396f97
jedmonson <jedmonson1478@gmail.com>
2021-07-29 14:06:10 -0400

    adding skeleton

---
+++ project-data-service/src/main/java/com/bah/msd/projectdataservice/ProjectDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.projectdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDataServiceApplication.class, args);
+	}
+
+}
---
+++ project-data-service/src/test/java/com/bah/msd/projectdataservice/ProjectDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.msd.projectdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
