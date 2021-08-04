736ac5640fc65f6665b367e931721d19d5b0a2a4
jedmonson <jedmonson1478@gmail.com>
2021-08-03 22:35:38 -0400

    creating auth service package

---
+++ project-auth-service/src/main/java/com/bah/msd/projectauthservice/ProjectAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.projectauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAuthServiceApplication.class, args);
+	}
+
+}
---
+++ project-auth-service/src/test/java/com/bah/msd/projectauthservice/ProjectAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.msd.projectauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
